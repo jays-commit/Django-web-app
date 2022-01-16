093f114b2b849615443957feb51cba8eba808142
Jesse O <97202317+jays-commit@users.noreply.github.com>
2022-01-16 12:59:12 +0000

    example post feature to test functionality

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,10 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = \
+    [{
+        "title": "Django Tutorial ",
+        "author": "Corey Schafer",
+        "content": "Tutorial 1",
+        "date_posted": "16th Jan 2022",
+    },
+        {
+            "title": "Django features ",
+            "author": "Jane Archaic",
+            "content": "Tutorial 2",
+            "date_posted": "17th Jan 2022",
+        }
+
+
+    ]
 
 
 def home(request):
-    return render(request, "blog/home.html")
+    context = {"posts": posts}
+    return render(request, "blog/home.html", context)
 
 
 def about(request):
-    return HttpResponse("<h1>About</h1>")
+    return render(request, "blog/about.html")

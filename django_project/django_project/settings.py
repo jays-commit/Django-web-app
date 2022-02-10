dfb6ceeb735195fa5552f28d817a9d5961966a36
Jesse O <97202317+jays-commit@users.noreply.github.com>
2022-02-10 21:55:56 +0000

    log in and log out pages added

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -123,7 +123,11 @@ STATIC_URL = 'static/'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
+LOGIN_REDIRECT_URL = 'blog-home'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+

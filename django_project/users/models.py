9cfc621de19b87068145d9c41bc974f522f16dd5
Jesse O <97202317+jays-commit@users.noreply.github.com>
2022-02-15 00:22:36 +0000

    signals, user profile automatically created with user

---
+++ django_project/users/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.CharField(User, max_length=140)
+    image = models.ImageField(default="default.jpg", upload_to="profile_pics")
+
+    def __str__(self):
+        return f"{self.user.username} Profile"
+
+
+

a2bc71a3881bc09ebc9940a1b50331caf429e691
Jesse O <97202317+jays-commit@users.noreply.github.com>
2022-02-19 18:00:58 +0000

    pagination added

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -35,6 +35,7 @@ class PostListView(ListView):
     template_name = 'blog/home.html'  # <app>/<model>_<viewtype>.html
     context_object_name = 'posts'
     ordering = ['-date_posted']
+    paginate_by = 4
 
 
 class PostDetailView(DetailView):
@@ -75,8 +76,6 @@ class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
         if self.request.user == post.author:
             return True
         return False
-
-
 
 
 def about(request):

04da39ad8204acbe8b1f480694b456f6fff5d491
Jesse O <97202317+jays-commit@users.noreply.github.com>
2022-02-19 10:53:14 +0000

    delete function added to blog

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -3,6 +3,8 @@ from django.views.generic import ListView
 from django.views.generic import DetailView
 from django.views.generic import CreateView
 from django.views.generic import UpdateView
+from django.views.generic import DeleteView
+from django.contrib.auth.mixins import UserPassesTestMixin
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Post
 
@@ -49,9 +51,32 @@ class PostCreateView(LoginRequiredMixin, CreateView):
 
 
 # Update view optional might follow the twitter model of no editing
-class PostUpdateView(LoginRequiredMixin, UpdateView):
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
     fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = '/'
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+
 
 
 def about(request):

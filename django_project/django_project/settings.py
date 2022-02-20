e4c5411376aeffa691ff6a7d8feba8a9dc302f17
Jesse O <97202317+jays-commit@users.noreply.github.com>
2022-02-20 14:05:27 +0000

    email reset password functionality

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -127,6 +127,14 @@ LOGIN_REDIRECT_URL = 'blog-home'
 
 LOGIN_URL = 'login'
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'instagramjames@gmail.com'
+EMAIL_HOST_PASSWORD = 'amcmsmasdbfovnzm'
+
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
